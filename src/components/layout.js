4a7be4a76ee39f0924c8810f2ffb04dcf0571003
aaronlugo <aaron.lugo.hdz@gmail.com>
2020-12-07 14:41:20 -0600

    Changing Header Style

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,15 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle="DivElement Github History" />
       <div
         style={{
           margin: `0 auto`,
