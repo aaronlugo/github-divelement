190256851eec215d6cc27549ce5812cafa209eb1
aaronlugo <aaron.lugo.hdz@gmail.com>
2020-12-07 16:55:50 -0600

    Adding the commit table

--- src/components/commits.js
+++ src/components/commits.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react"
+import { Table } from "react-bootstrap"
 
 const CommitsData = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [commits, setCommits] = useState([]);
-  console.log('hey');
   useEffect(() => {
     fetch("https://api.github.com/repos/aaronlugo/github-divelement/commits")
       .then(res => res.json())
@@ -12,7 +12,6 @@ const CommitsData = () => {
         (result) => {
           setIsLoading(false);
           setCommits(() => result);
-          console.log(result, commits)
         },
         (error) => {
           setIsLoading(false);
@@ -28,7 +27,26 @@ const CommitsData = () => {
   } else {
     console.log('comm', commits)
     return (
-      <p>Commits</p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>SHA</th>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {commits.map((c, index) => 
+            <tr key={index}>
+              <td>{c.commit.author.name}</td>
+              <td>{c.sha}</td>
+              <td>{c.commit.author.date}</td>
+              <td>{c.commit.message}</td>
+            </tr>
+            )}
+        </tbody>
+      </Table>
     );
   }
 }
--- src/pages/Commits.js
+++ src/pages/Commits.js
@@ -4,14 +4,18 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CommitsData from "../components/commits"
+import {Container} from "react-bootstrap"
 
 const Commits = () =>
  (
     <Layout>
       <SEO title="Commits" />
-      <h1>Commits History</h1>
-      <CommitsData />
-      <Link to="/">Go back to the homepage</Link>
+      <Container>
+        <h1>Commits History</h1>
+        <CommitsData />
+        <Link to="/">Go back to the homepage</Link>
+      </Container>
+      
     </Layout>
   )
 
